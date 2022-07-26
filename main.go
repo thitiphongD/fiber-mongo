e7cbb41e0dbe95fbb6bcb9969cbc97845cd5b2d9
thitiphongD <thitipsr1504@gmail.com>
2022-07-26 17:02:23 +0700

    add controller

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/thitiphongD/fiber-mongo/configs"
+	"github.com/thitiphongD/fiber-mongo/routes"
 )
 
 func main() {
 	app := fiber.New()
 
 	configs.ConnectDB()
+
+	routes.UserRoute(app)
 
 	app.Listen(":6000")
 }
